7445a4cffdc6241a381b5deaee48b79df0b80ad8
KangHayeonn <rkdgkdus522@naver.com>
2021-08-03 12:04:19 +0900

    test_commit

--- src/Main/MainScreen.java
+++ src/Main/MainScreen.java
@@ -7,9 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+
 
 public class MainScreen {
 	private JFrame frame;
